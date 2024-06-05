27586e00332f7d870d726f7d0894c9d4bab6b034
Sherzod0001 <141539590+Sherzod0001@users.noreply.github.com>
2024-06-05 21:50:00 +0500

    Times-table first commit

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Runtime.Intrinsics.Arm;
+Console.WriteLine("Bu dastur siz kiritgan raqamlarning oralig'idagi karra jadvalini chiqarib beradi : (3 6)");
+int start = Convert.ToInt32(Console.ReadLine());
+int finish = Convert.ToInt32(Console.ReadLine());
+
+for (int row = 1; row <= 10; row++)
+{
+    for (int column = start; column <= finish; column++)
+    {
+        Console.Write($"{column} * {row} = {row * column} \t");
+    }
+    Console.WriteLine();
+}
+
+
+
+
+
+
+
+// for(int i=1;i<11;i++){
+//     for(int j=1;j<11;j++){
+//         Console.Write($"{j} * {i} = {i*j} \t");
+
+//     }
+//     Console.WriteLine();
+// }
